473a162fa6f2b24339ed368bc357bd2dfdb56eee
amchinecherem <chinecheremkfr@gmail.com>
2023-09-07 16:11:54 -0700

    lovely

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - prints the exact word in a line
+ * Return: Always 0 if it's successful
+*/
+int main(void)
+{
+write(3, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 10);
+return (1);
