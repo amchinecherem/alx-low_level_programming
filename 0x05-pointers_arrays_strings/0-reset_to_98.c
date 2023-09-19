b52285885f487475439ddda964202b7d05341c9d
amchinecherem <chinecheremkfr@gmail.com>
2023-09-19 14:27:11 -0700

    reset to 98 added

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98-updates the value of the variables
+ * the pointer points to 98
+ * @n:pointer to variable to the update
+ */
+
+void reset_to_98(int *n){
+	*n = 98;
+}
