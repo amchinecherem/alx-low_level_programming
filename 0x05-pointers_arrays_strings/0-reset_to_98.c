baa89c6e097593580956a34b5d0bdaa8a079bcfb
amchinecherem <chinecheremkfr@gmail.com>
2023-09-19 14:34:54 -0700

    98 commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,8 +3,9 @@
  * reset_to_98-updates the value of the variables
  * the pointer points to 98
  * @n:pointer to variable to the update
+ * Return: Always 0
  */
-
-void reset_to_98(int *n){
-	*n = 98;
+void reset_to_98(int *n)
+{
+*n = 98;
 }
