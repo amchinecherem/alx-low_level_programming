14d7b42df107c8fd324b435ca7738c2bff1f1921
amchinecherem <chinecheremkfr@gmail.com>
2023-09-19 18:09:11 -0700

    half string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - prints one char out of 2 of a string
+ * puts_half - prints one char out of 2 of a string
  * followed by a new line of code again
  * @str: string to print the chars from
  */
@@ -14,7 +14,7 @@ void puts_half(char *str)
 	while (str[full_string] != '\0')
 		full_string++;
 
-	half_string = full_string/2;
+	half_string = full_string / 2;
 	if (full_string % 2 == 1)
 		half_string++;
 	while (half_string < full_string)
